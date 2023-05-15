445740f279b73b29036888166798522b5d0742c9
Katie Sandfort <katie.sandfort@gmail.com>
2023-05-15 08:54:21 -0700

    refactors Head to fix issue with body segments

--- src/Head.js
+++ src/Head.js
@@ -1,67 +1,61 @@
 class Head {
-    constructor(el) {
-        this.board = el; //board on the DOM
+  constructor(el) {
+    this.board = el; // board on the DOM
+    this.node = document.createElement('div');
+    this.node.setAttribute('id', 'head');
+    el.appendChild(this.node);
 
-        //create the head on the board
-        this.node = document.createElement('div');
-        this.node.setAttribute('id', 'head');
-        el.appendChild(this.node);
+    //initialze current direction as null to prevent movement until user provides direction
+    this.currentDirection = 'null';
+    this.SPEED = 250; // time interval in milliseconds
 
-        //set direction to null as default so snake doesn't start moving until key press
-        this.currentDirection = 'null';
-        this.SPEED = 250; //set time interval in milliseconds
+    //set starting location of the snake head as the top left corner of the board
+    this.node.style.top = '0px';
+    this.node.style.left = '0px';
 
+    //track the previous positions of the head
+    this.headPosition = [];
 
-        //set starting position of head to top-left corner of the board
-        this.node.style.top = '0px';
-        this.node.style.left = '0px';
+    //maintain all the non-head body parts
+    this.bodyParts = [];
 
-        // track the previous positions of the head 
-        this.headPosition = [];
-
-        //maintain all the non-head body parts
-        this.bodyParts = [];
-
-        // Trigger the first invocation of move().
-        // Remember to bind 'this' to move(), so that move() can point to (via 'this') the instance of Head that called it.
-        // Otherwise, 'this' in move() will be the Windows object.
-        setTimeout(this.move.bind(this), this.SPEED);
-    }
-
+    //start the game
+    setTimeout(this.move.bind(this), this.SPEED);
+  }
 
   move() {
     //continue to loop the game
-    const moveTimeoutID = setTimeout(this.move.bind(this), this.SPEED);
+    const timeoutID = setTimeout(this.move.bind(this), this.SPEED);
 
     const head = this.node;
     const direction = this.currentDirection;
-    
-    // determine the top and left psoitions of the head
-    let topPosition = Number(head.style.top.replace('px', ''))
-    let leftPosition = Number(head.style.left.replace('px', ''))
 
-    // change direction as user provides input
-    switch (direction) {
-      case 'right':
-        head.style.left = `${leftPosition += 50}px`;
-        break;
+    //determine the top and left positions of the head
+    let topPosition = Number(head.style.top.replace('px', ''));
+    let leftPosition = Number(head.style.left.replace('px', ''));
+
+    //change direction as the user provides directional input
+    switch(direction) {
       case 'left':
-        head.style.left = `${leftPosition -= 50}px`;
+        head.style.left = `${(leftPosition -= 50)}px`;
         break;
-      case 'up':
-        head.style.top = `${topPosition -= 50}px`;
+      case 'right':
+        head.style.left = `${(leftPosition += 50)}px`;
         break;
       case 'down':
-        head.style.top = `${topPosition += 50}px`;
+        head.style.top = `${(topPosition += 50)}px`;
         break;
+      case 'up':
+        head.style.top = `${(topPosition -= 50)}px`; 
+        break; 
     }
 
-    //end game if head moves out of bounds
+    //terminate game if out of bounds
     if (this.headIsOutBounds(topPosition, leftPosition)){
       return this.endGame(timeoutID);
     }
-
-    if (this.appleIsEaten(topPosition, leftPosition)) {
+    
+    if(this.appleIsEaten(topPosition, leftPosition)){
       //clear current apple
       this.removeApple();
       //generate a new apple
@@ -72,67 +66,65 @@ class Head {
       this.SPEED -= 10;
     }
 
-    //enqueue head positions to the headPositions array, and ensure it's only one element longer than the bodyParts array
+    //enqueue head positions to headPositions array, and ensure it's only one element longer than bodyParts array
     this.trackHeadPositions(topPosition, leftPosition);
 
     //if any body pieces exist, move them to the next position
-    if (this.bodyParts.length > 0) {
+    if (this.bodyParts.length > 0){
       this.moveBody(timeoutID);
     }
   }
 
-  headIsOutBounds(topPosition, leftPosition) {
+  headIsOutBounds(topPosition, leftPosition){
     return topPosition >= 700 || topPosition < 0 || leftPosition >= 700 || leftPosition < 0;
   }
 
-  appleIsEaten(topPosition, leftPosition) {
+  appleIsEaten(topPosition, leftPosition){
     const apple = document.querySelector('#apple').style;
 
     return (`${topPosition}px` === apple.top && `${leftPosition}px` === apple.left)
   }
-
+  
   removeApple() {
     const apple = document.querySelector('#apple');
 
     apple.remove();
   }
-
+ 
   generateApple(){
     let newApple;
     let appleLocationIsInvalid = true;
 
-    while (appleLocationIsInvalid) {
-      if(newApple) {
+    while(appleLocationIsInvalid){
+      if(newApple){
         this.removeApple();
       }
       new Apple(this.board);
       newApple = document.querySelector('#apple');
 
-      appleLocationisInvalid = this.headPosition.some((top, left) => {
+      appleLocationIsInvalid = this.headPosition.some((top, left) =>{
         return (`${top}px` === newApple.style.top && `${left}px` === newApple.style.left)
       });
     }
   }
-  
   generateNewBody(){
     const newBody = new Body(this.board);
     this.bodyParts.push(newBody);
   }
 
-  trackHeadPositions(topPosition, leftPosition) {
-    //add current head position to the front of headPositions
+  trackHeadPositions(topPosition, leftPosition){
+    //add the current head position to the front of headPositions
     this.headPosition.unshift({top: topPosition, left: leftPosition});
 
-    if(this.headPosition.length > this.bodyParts.length + 1) this.headPosition.pop()
+    if(this.headPosition.length > this.bodyParts.length + 1) this.headPosition.pop();
   }
 
   moveBody(timeoutID){
     //get current head position to determine where to move the body parts
     const {top: headTop, left: headLeft} = this.headPosition[0];
 
-    for (let i = 0; i < this.bodyParts.length; i++) {
+    for (let i = 0; i < this.bodyParts.length; i++){
       const currentBody = this.bodyParts[i].node.style;
-
       if(`${headTop}px` === currentBody.top && `${headLeft}px` === currentBody.left){
         return this.endGame(timeoutID);
       }
@@ -140,15 +132,13 @@ class Head {
       const {top: newTop, left: newLeft} = this.headPosition[i + 1];
       currentBody.top = `${newTop}px`;
       currentBody.left = `${newLeft}px`;
-
-
     }
   }
 
-  endGame(timeoutID) {
+  endGame(timeoutID){
     clearTimeout(timeoutID);
 
-    confirm('Do you want to play again?');
+    confirm('Do you want to play the game again?');
 
     document.querySelectorAll('.body').forEach(node => node.remove());
 
