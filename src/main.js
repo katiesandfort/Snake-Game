ea717b68d57a403d9b7ed13574e98ce0ee2ffd93
Katie Sandfort <katie.sandfort@gmail.com>
2023-05-15 08:48:08 -0700

    removed difficulty modal

--- src/main.js
+++ src/main.js
@@ -7,66 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
     //create new head and apple element
     const head = new Head(board);
     new Apple(board);
-    const modal = document.createElement('div');
-  modal.setAttribute('id', 'game-start-modal');
-  modal.innerText = 'Select a difficulty to start game:\n';
-
-  const easyBtn = document.createElement('button');
-  easyBtn.setAttribute('id', 'easyBtn');
-  easyBtn.innerText = 'EASY';
-  easyBtn.onclick = function() {
-    head.SPEED = 500;
-    modal.style.display = 'none';
-  }
   
-  const medBtn = document.createElement('button');
-  medBtn.setAttribute('id', 'medBtn');
-  medBtn.innerText = 'MEDIUM';
-  medBtn.onclick = function() {
-    head.SPEED = 250;
-    modal.style.display = 'none';
-  }
 
-  const hardBtn = document.createElement('button');
-  hardBtn.setAttribute('id', 'hardBtn');
-  hardBtn.innerText = 'HARD';
-  hardBtn.onclick = () => {
-    head.SPEED = 100;
-    modal.style.display = 'none';
-  }
-
-  const insaneBtn = document.createElement('button');
-  insaneBtn.setAttribute('id', 'insaneBtn');
-  insaneBtn.innerText = 'INSANE';
-  insaneBtn.onclick = () => {
-    head.SPEED = 40;
-    modal.style.display = 'none';
-  }
-
-  modal.append(easyBtn, medBtn, hardBtn, insaneBtn);
-  body.append(modal);
-
-  // Listen to 'keydown' event occurring in <body>.
+  
   body.addEventListener('keydown', (e) => {
-    // Clear away modal if user wants to start game by pressing an arrow key
-    // without clicking on a difficulty. Default speed is 250 (medium).
-    modal.style.display = 'none';
-
-    const currDir = head.currentDirection;
-
-    switch (e.code) {
-      case 'ArrowLeft':
-        if (currDir !== 'right') head.currentDirection = 'left';
-        break;
-      case 'ArrowRight':
-        if (currDir !== 'left') head.currentDirection = 'right';
-        break;
-      case 'ArrowUp':
-        if (currDir !== 'down') head.currentDirection = 'up';
-        break;
-      case 'ArrowDown':
-        if (currDir !== 'up') head.currentDirection = 'down';
-        break;
+    //move head based on direction from arrow keys, and ensure it can't go backwards
+    if (e.code === 'ArrowLeft' && head.currentDirection !== 'right') {
+      head.currentDirection = 'left';
+    }
+    if (e.code === 'ArrowRight' && head.currentDirection !== 'left') {
+      head.currentDirection = 'right';
+    }
+    if (e.code === 'ArrowDown' && head.currentDirection !== 'up') {
+      head.currentDirection = 'down';
+    }
+    if (e.code === 'ArrowUp' && head.currentDirection !== 'down') {
+      head.currentDirection = 'up';
     }
   })
 });
